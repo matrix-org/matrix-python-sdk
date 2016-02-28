d991fc4b6259edd0790450632a121316d28673b5
Will Hunt <half-shot@molrams.com>
2016-02-28 16:01:10 +0000

    Added help text for script.

--- samples/startSample.sh
+++ samples/startSample.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ "$1" == "-h" ] || [ $# -ne 1 ]; then
+  echo "Usage: `basename $0` scriptname [OPTIONS] "
+  exit 0
+fi
+
 # Will run a sample without requiring an install of the library.
 PYTHONPATH="$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ))"
 export PYTHONPATH=$PYTHONPATH
