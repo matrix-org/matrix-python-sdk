e7d48b612b62282a2b673540a4eb04f27211c6d2
Will Hunt <half-shot@molrams.com>
2016-03-21 13:55:46 +0000

    Fixed wrong msgtype for send_image

--- matrix_client/client.py
+++ matrix_client/client.py
@@ -183,7 +183,7 @@ class Room(object):
 
     #See http://matrix.org/docs/spec/r0.0.1/client_server.html#m-image for the imageinfo args.
     def send_image(self, url, name, **imageinfo):
-        return self.client.api.send_content(self.room_id, url, name, "image", imageinfo)
+        return self.client.api.send_content(self.room_id, url, name, "m.image", imageinfo)
 
     def add_listener(self, callback):
         self.listeners.append(callback)
