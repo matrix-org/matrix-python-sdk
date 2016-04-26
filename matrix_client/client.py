056c9a1b8e4dfa32821c31d68a40c15e57a997d2
Will Hunt <half-shot@molrams.com>
2016-04-26 17:16:28 +0100

    Put function parameters on a new line.

--- matrix_client/client.py
+++ matrix_client/client.py
@@ -192,11 +192,10 @@ class Room(object):
     # See http://matrix.org/docs/spec/r0.0.1/client_server.html#m-image for the
     # imageinfo args.
     def send_image(self, url, name, **imageinfo):
-        return self.client.api.send_content(self.room_id,
-                                            url,
-                                            name,
-                                            "m.image",
-                                            imageinfo)
+        return self.client.api.send_content(
+            self.room_id, url, name, "m.image",
+            extra_information=imageinfo
+        )
 
     def add_listener(self, callback):
         self.listeners.append(callback)
