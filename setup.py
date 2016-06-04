549aa0ac39f84cf7e0ae94c8041dc9e70a4fb89b
Severen Redwood <severen@shrike.me>
2016-06-04 23:28:08 +1200

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 
 
-def read_file(names, encoding="utf-8"):
+def read_file(names, encoding='utf-8'):
     file_path = os.path.join(here, *names)
     if encoding:
         with codecs.open(file_path, encoding=encoding) as f:
             return f.read()
     else:
-        with open(file_path, "rb") as f:
+        with open(file_path, 'rb') as f:
             return f.read()
 
 
@@ -24,24 +24,27 @@ def exec_file(names):
 
 
 setup(
-    name="matrix_client",
-    version=exec_file(("matrix_client", "__init__.py",))["__version__"],
-    description="Client-Server SDK for Matrix",
-    long_description=read_file(("README.rst",)),
-    author="matrix.org",
-    author_email="team@matrix.org",
-    url="https://github.com/matrix-org/matrix-python-sdk",
+    name='matrix_client',
+    version=exec_file(('matrix_client', '__init__.py',))['__version__'],
+    description='Client-Server SDK for Matrix',
+    long_description=read_file(('README.rst')),
+    author='The Matrix Developers (matrix.org)',
+    author_email='team@matrix.org',
+    url='https://github.com/matrix-org/matrix-python-sdk',
     packages=['matrix_client'],
-    license="Apache License, Version 2.0",
+    license='Apache License, Version 2.0',
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 2",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Communications :: Chat',
+        'Topic :: Communications :: Conferencing',
     ],
-    keywords="matrix matrix.org",
+    keywords='chat sdk matrix matrix.org',
     install_requires=[
-        "requests"
+        'requests'
     ],
     extras_require={
         'test': ['tox', 'pytest', 'flake8']
