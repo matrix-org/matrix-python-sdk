72c83d83a015aefb1dd8f5816defc3a9c4f0c516
jan <janp@stanford.edu>
2016-10-17 22:54:22 -0700

    Add method for setting the room name

--- matrix_client/room.py
+++ matrix_client/room.py
@@ -207,6 +207,20 @@ class Room(object):
         except MatrixRequestError:
             return False
 
+    def set_room_name(self, name):
+        """ Set room name
+            name (str): The new name for the room
+
+        Returns:
+            boolean: True if the name changed, False if not
+        """
+        try:
+            self.client.api.set_room_name(self.room_id, name)
+            self.name = name
+            return True
+        except MatrixRequestError:
+            return False
+
     def send_state_event(self, event_type, content, state_key):
         """ Send a state event to the room.
 
