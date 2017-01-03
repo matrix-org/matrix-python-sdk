matrix-org/matrix-python-sdk

commit ad330ae44e27eef4ab01da0ee4b28ca8b8c8398c

matrix_client: Add support for ephemeral events
Ephemeral events will now be broadcasted to global ephemeral listeneres,
and pushed to their respected rooms in order to inform all their
ephemeral listeners.
Listeners can pass a filter for specific event types, or leave it empty to
receive all events.

Issue: #54
Signed-off-by: Gal Pressman <galpressman@gmail.com>

matrix_client/room.py
@@ -22,6 +22,7 @@ class Room(object):
         self.client = client
         self.listeners = []
         self.state_listeners = []
+        self.ephemeral_listeners = []
         self.events = []
         self.event_history_limit = 20
         self.name = None
@@ -96,6 +97,20 @@ class Room(object):
             }
         )
 
+    def add_ephemeral_listener(self, callback, event_type=None):
+        """ Add a callback handler for ephemeral events going to this room.
+
+        Args:
+            callback (func(roomchunk)): Callback called when an ephemeral event arrives.
+            event_type (str): The event_type to filter for.
+        """
+        self.ephemeral_listeners.append(
+            {
+                'callback': callback,
+                'event_type': event_type
+            }
+        )
+
     def add_state_listener(self, callback, event_type=None):
         """ Add a callback handler for state events going to this room.
 
@@ -117,6 +132,12 @@ class Room(object):
 
         # Dispatch for room-specific listeners
         for listener in self.listeners:
+            if listener['event_type'] is None or listener['event_type'] == event['type']:
+                listener['callback'](self, event)
+
+    def _put_ephemeral_event(self, event):
+        # Dispatch for room-specific listeners
+        for listener in self.ephemeral_listeners:
             if listener['event_type'] is None or listener['event_type'] == event['type']:
                 listener['callback'](self, event)
 

matrix_client/client.py
@@ -86,6 +86,7 @@ class MatrixClient(object):
         self.listeners = []
         self.invite_listeners = []
         self.left_listeners = []
+        self.ephemeral_listeners = []
 
         self.sync_token = None
         self.sync_filter = None
@@ -210,6 +211,21 @@ class MatrixClient(object):
             event_type (str): The event_type to filter for.
         """
         self.listeners.append(
+            {
+                'callback': callback,
+                'event_type': event_type
+            }
+        )
+
+    def add_ephemeral_listener(self, callback, event_type=None):
+        """ Add an ephemeral listener that will send a callback when the client recieves
+        an ephemeral event.
+
+        Args:
+            callback (func(roomchunk)): Callback called when an ephemeral event arrives.
+            event_type (str): The event_type to filter for.
+        """
+        self.ephemeral_listeners.append(
             {
                 'callback': callback,
                 'event_type': event_type
@@ -362,6 +378,17 @@ class MatrixClient(object):
 
                 # Dispatch for client (global) listeners
                 for listener in self.listeners:
+                    if (
+                        listener['event_type'] is None or
+                        listener['event_type'] == event['type']
+                    ):
+                        listener['callback'](event)
+
+            for event in sync_room['ephemeral']['events']:
+                event['room_id'] = room_id
+                room._put_ephemeral_event(event)
+
+                for listener in self.ephemeral_listeners:
                     if (
                         listener['event_type'] is None or
                         listener['event_type'] == event['type']

commit 4bc26b295fa93f8b173fe793d5702b252896b9ae

room.py: Fix room's add listeners docstring
Room listeners callbacks are called with two parameters, the first
one is the room object and the second is the event itself.
Docstring stated callbacks as one parameter functions, this patch
aligns it to the code.

Fixes: ad330ae44e27 ("matrix_client: Add support for ephemeral events")
Fixes: f160ec459cdc ("Extract Room, User to separate files from client")
Signed-off-by: Gal Pressman <galpressman@gmail.com>

matrix_client/room.py
@@ -117,7 +117,7 @@ class Room(object):
         """ Add a callback handler for events going to this room.
 
         Args:
-            callback (func(roomchunk)): Callback called when an event arrives.
+            callback (func(room, event)): Callback called when an event arrives.
             event_type (str): The event_type to filter for.
         """
         self.listeners.append(
@@ -131,7 +131,7 @@ class Room(object):
         """ Add a callback handler for ephemeral events going to this room.
 
         Args:
-            callback (func(roomchunk)): Callback called when an ephemeral event arrives.
+            callback (func(room, event)): Callback called when an ephemeral event arrives.
             event_type (str): The event_type to filter for.
         """
         self.ephemeral_listeners.append(