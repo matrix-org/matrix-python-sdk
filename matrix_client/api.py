b741c46913eca0454207683692eee3e9dfb4ed0a
pik <alexander.maznev@gmail.com>
2017-03-10 09:30:22 -0300

    Fix flake8 for api filter methods
    Signed-off-by: pik <alexander.maznev@gmail.com>

--- matrix_client/api.py
+++ matrix_client/api.py
@@ -26,6 +26,7 @@ except ImportError:
 
 MATRIX_V2_API_PATH = "/_matrix/client/r0"
 
+
 class MatrixHttpApi(object):
     """Contains all raw Matrix HTTP Client-Server API calls.
 
@@ -449,17 +450,15 @@ class MatrixHttpApi(object):
         }
 
     def get_filter(self, user_id, filter_id):
-        return self._send("GET",
-            "/user/{userId}/filter/{filterId}".format(userId=user_id, filterId=filter_id),
-            api_path=MATRIX_V2_API_PATH
-        )
+        return self._send("GET", "/user/{userId}/filter/{filterId}"
+                          .format(userId=user_id, filterId=filter_id),
+                          api_path=MATRIX_V2_API_PATH)
 
     def create_filter(self, user_id, filter_params):
         return self._send("POST",
-            "/user/{userId}/filter".format(userId=user_id),
-            filter_params,
-            api_path=MATRIX_V2_API_PATH
-        )
+                          "/user/{userId}/filter".format(userId=user_id),
+                          filter_params,
+                          api_path=MATRIX_V2_API_PATH)
 
     def _send(self, method, path, content=None, query_params={}, headers={},
               api_path="/_matrix/client/api/v1"):
