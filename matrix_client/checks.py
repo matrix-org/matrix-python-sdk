50ca4c6442779f0599ccac1dba16a7ad914ac405
Adam Beckmeyer <adam_gpg@thebeckmeyers.xyz>
2018-05-22 10:59:16 -0400

    Fix PEP8 violation
    Signed-off-by: Adam Beckmeyer <adam_gpg@thebeckmeyers.xyz>

--- matrix_client/checks.py
+++ matrix_client/checks.py
@@ -12,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
 def check_room_id(room_id):
     if not room_id.startswith("!"):
         raise ValueError("RoomIDs start with !")
