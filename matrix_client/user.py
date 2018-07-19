6b9232d0d3b63eb75fe98aeafd44471b10e320bf
Abhishek Kumar <abhishekkumar2718@gmail.com>
2018-07-19 22:59:41 +0530

    Forgot to import MatrixRequestError

--- matrix_client/user.py
+++ matrix_client/user.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from .checks import check_user_id
+from .errors import MatrixRequestError
 
 
 class User(object):
