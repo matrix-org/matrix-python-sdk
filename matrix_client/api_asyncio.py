735523adcb07b3aea116460e32892e839ace664a
Stuart Mumford <stuart@cadair.com>
2018-11-05 22:04:14 +0000

    We will never hit this

--- matrix_client/api_asyncio.py
+++ matrix_client/api_asyncio.py
@@ -1,7 +1,8 @@
 import sys
 
 if sys.version_info < (3, 5):
-    raise ValueError("The asyncio version of the api is only supported on Python 3.5+")
+    raise ValueError("The asyncio version of the api "
+                     "is only supported on Python 3.5+")  # pragma: nocover
 else:
     from ._api_asyncio import AsyncHTTPAPI
 
